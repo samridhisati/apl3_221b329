901223facacbda36cd2c93653f269dd14e196dc7
221B329 <221B329@juet.co.in>
2025-01-15 15:25:04 +0530

    labexercise commit

---
+++ Lab0_329/Lab_0.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Lab_0 {
+    private String name;
+    private int age;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
---
+++ Lab0_329/Main (1).java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Lab_0 person = new Lab_0();
+        person.setName("Samridhi");
+        person.setAge(20);
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+    }
+}
