a130922b711d2514251a03511f40868e379fecf4
221B329 <221B329@juet.co.in>
2025-01-29 14:44:33 +0530

    Lab Exercise 2

---
+++ LE2_329/prob1/Voice.java
@@ -0,0 +1,15 @@
+class Voice{
+    Animal[] animal =new Animal[5];
+    void prepareVoice(){
+        animal[0]=new Cow();
+        animal[1]=new Dog();
+        animal[2]=new Pig();
+        animal[3]=new Goat();
+        animal[4]=new Lion();
+    }
+    void hear(){
+        for(int i=0;i<5;i++){
+            animal[i].makeVoice();
+        }
+    }
+}
